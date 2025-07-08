5b9bfb5a765cab1f67e0a0e8adfcc7c4d0f7443d
kreshma <kolkarkreshma@gmail.com>
2025-07-08 15:12:57 +0530

    third commit

---
+++ HYR_Tutorials_java/src/practice/Datatypes - Copy.java
@@ -0,0 +1,30 @@
+package practice;
+
+public class Datatypes {
+	public static void main(String[] args) {
+		//System.out.println(Byte.SIZE / 8);
+		byte b=127;
+		short s=200;
+		int i=2146798347;
+		long l=6587843798327L;
+		double d=88.65745;
+		float f=3.14f;
+		char c='d';
+		boolean b1=true;
+		boolean b2=false;
+		//here you can check min and max values of that particular data type.
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+//		System.out.println(Short.SIZE / 8);
+//		System.out.println(Long.SIZE / 8);
+//		System.out.println(Double.SIZE / 8);
+//		System.out.println(Float.SIZE / 8);
+//		System.out.println(Character.SIZE / 8);
+//		//System.out.println(Integer.SIZE / 8);
+
+		//Written by reshma;
+	}
+
+}
